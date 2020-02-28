6bb82a830464fbeb86f2bd8a22724ef323af1a11
aymankhawaldeh <runner@fv-az83.4bqojh5guq4unaknv1nhaa2xrg.bx.internal.cloudapp.net>
2020-02-28 20:29:00 +0000

    Updates

---
+++ precache-manifest.9792d82a1147236cdace495bae42d406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a001bb0f74410e082a24d99cd9b53eb",
+    "url": "/ayman-401-advanced-javascript/lab27.io/lab27/index.html"
+  },
+  {
+    "revision": "facdb0c48936ca7a5d96",
+    "url": "/ayman-401-advanced-javascript/lab27.io/lab27/static/css/main.6775166d.chunk.css"
+  },
+  {
+    "revision": "a0ec8bb6e65f3af7997f",
+    "url": "/ayman-401-advanced-javascript/lab27.io/lab27/static/js/2.b0c61cd0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ayman-401-advanced-javascript/lab27.io/lab27/static/js/2.b0c61cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "facdb0c48936ca7a5d96",
+    "url": "/ayman-401-advanced-javascript/lab27.io/lab27/static/js/main.8ddff269.chunk.js"
+  },
+  {
+    "revision": "8fb4684bc7544a12d8bb",
+    "url": "/ayman-401-advanced-javascript/lab27.io/lab27/static/js/runtime-main.c2e2e977.js"
+  }
+]);
